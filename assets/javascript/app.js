7c7762e25e1851350a902d455e63cc4521dd6c3e
Guillaume Paugam <guipaugam@icloud.com>
2018-03-15 20:52:56 -0500

    Cleaned up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,25 @@
 let randomVerb = verbArray[Math.floor(Math.random()*verbArray.length)];
 // let randomNoun = nounArray[Math.floor(Math.random()*nounArray.length)];
 
-let randomVerbAnswers = [];
+let randomVerbTrans = [];
 // let randomNounAnswers = [];
-randomVerbAnswers.push(randomVerb.englishTrans,randomVerb.badEnglishTrans1,randomVerb.badEnglishTrans2,randomVerb.badEnglishTrans3,randomVerb.badEnglishTrans4); 
+randomVerbTrans.push(randomVerb.englishTrans,randomVerb.badEnglishTrans1,randomVerb.badEnglishTrans2,randomVerb.badEnglishTrans3,randomVerb.badEnglishTrans4); 
 // randomNounAnswers.push(randomNoun.englishTrans,randomNoun.badEnglishTrans1,randomNoun.badEnglishTrans2); 
 
-var options = randomVerbAnswers; 
-randomCuesArray =[];
-
+randomizedTrans =[];
 
 function cut (array) {
     option = array[Math.floor(Math.random()*array.length)];
-    console.log("firstoption: ", option);
     for (let i = 0; i < array.length; i++) {
         if(option === array[i]) {
-            console.log("original array: ", options, array[i])
             array.splice(i,1);
-            console.log("array after splicing: ", array)
-            randomCuesArray.push(option);
-            console.log("cues array after pushing: ", randomCuesArray)
+            randomizedTrans.push(option);
         }
     };
 };
-// cut(options);
 let j = 0;
-while (j = 0 < options.length) {
-    cut(options)
+while (j = 0 < randomVerbTrans.length) {
+    cut(randomVerbTrans)
     j++;
 }
 
-// cut(options);
-// cut(options);
-// cut(options);
-// cut(options);
-// cut(options);
