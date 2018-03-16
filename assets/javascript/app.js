97caecbf6880e120e7e858ab0420ffdfc0e84feb
Guillaume Paugam <guipaugam@icloud.com>
2018-03-15 20:43:41 -0500

    While loop to randomize answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,36 +6,32 @@ let randomVerbAnswers = [];
 randomVerbAnswers.push(randomVerb.englishTrans,randomVerb.badEnglishTrans1,randomVerb.badEnglishTrans2,randomVerb.badEnglishTrans3,randomVerb.badEnglishTrans4); 
 // randomNounAnswers.push(randomNoun.englishTrans,randomNoun.badEnglishTrans1,randomNoun.badEnglishTrans2); 
 
-let firstOption = randomVerbAnswers[Math.floor(Math.random()*randomVerbAnswers.length)];
-for (let i = 0; i < randomVerbAnswers.length; i++) {
-    if (firstOption === randomVerbAnswers[i]) {
-        randomVerbAnswers.splice(i,1);
-    }
-}
-let secondOption = randomVerbAnswers[Math.floor(Math.random()*randomVerbAnswers.length)];
-    for (let i = 0; i < randomVerbAnswers.length; i++) {
-    if (secondOption === randomVerbAnswers[i]) {
-        randomVerbAnswers.splice(i,1);
-    }
-}
- 
-let thirdOption = randomVerbAnswers[Math.floor(Math.random()*randomVerbAnswers.length)];
-for (let i = 0; i < randomVerbAnswers.length; i++) {
-    if (thirdOption === randomVerbAnswers[i]) {
-        randomVerbAnswers.splice(i,1);
-    }
-}
-let fourthOption = randomVerbAnswers[Math.floor(Math.random()*randomVerbAnswers.length)];
-    for (let i = 0; i < randomVerbAnswers.length; i++) {
-    if (fourthOption === randomVerbAnswers[i]) {
-        randomVerbAnswers.splice(i,1);
-    }
-}
-fifthOption = randomVerbAnswers.toString();
+var options = randomVerbAnswers; 
+randomCuesArray =[];
 
 
-console.log("Option #1: ",firstOption)
-console.log("Option #2: ",secondOption)
-console.log("Option #3: ",thirdOption)
-console.log("Option #4: ",fourthOption)
-console.log("Option #5: ",fifthOption)
+function cut (array) {
+    option = array[Math.floor(Math.random()*array.length)];
+    console.log("firstoption: ", option);
+    for (let i = 0; i < array.length; i++) {
+        if(option === array[i]) {
+            console.log("original array: ", options, array[i])
+            array.splice(i,1);
+            console.log("array after splicing: ", array)
+            randomCuesArray.push(option);
+            console.log("cues array after pushing: ", randomCuesArray)
+        }
+    };
+};
+// cut(options);
+let j = 0;
+while (j = 0 < options.length) {
+    cut(options)
+    j++;
+}
+
+// cut(options);
+// cut(options);
+// cut(options);
+// cut(options);
+// cut(options);
