1d7fc3b7955f0b1780e7a75061dbee760e329d5f
Guillaume Paugam <guipaugam@icloud.com>
2018-03-16 21:03:57 -0500

    Function work continued

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-let randomVerb = [] 
+let randomVerb 
 let randomVerbTrans = [];
 let randomizedTrans =[];
+$("#question_panel").hide();
 
 
 function newRiddle(){
@@ -11,11 +12,31 @@ function newRiddle(){
     randomVerbTrans.push(randomVerb.englishTrans,randomVerb.badEnglishTrans1,randomVerb.badEnglishTrans2,randomVerb.badEnglishTrans3,randomVerb.badEnglishTrans4); 
     // randomNounAnswers.push(randomNoun.englishTrans,randomNoun.badEnglishTrans1,randomNoun.badEnglishTrans2); 
     timer();
+    console.log(randomVerb)
     let j = 0;
     while (j = 0 < randomVerbTrans.length) {
         cut(randomVerbTrans)
         j++;
     }
+    console.log(randomizedTrans);
+    $("#question_panel").show();
+    $("#question").val(randomVerb.frenchTrans).text(randomVerb.frenchTrans)
+    $("#1").val(randomizedTrans[0]).text(randomizedTrans[0])
+    $("#2").val(randomizedTrans[1]).text(randomizedTrans[1])
+    $("#3").val(randomizedTrans[2]).text(randomizedTrans[2])
+    $("#4").val(randomizedTrans[3]).text(randomizedTrans[3])
+    $("#5").val(randomizedTrans[4]).text(randomizedTrans[4])
+// need to get the value of the li element clicked (currently, always shows game over, meaning it doesn't read anything)
+    $("li").on("click", function() {
+        console.log("Click recorded")
+        if ($("#option").val() === randomVerb.englishTrans){
+            gameWon();
+        }
+        else {
+            gameLost();
+        }
+    });
+
 }    
 function timer(){
       var counter = 11;
