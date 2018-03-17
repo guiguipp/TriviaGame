4f8057712a6b0c6d3b8501337953ce664d781aaf
Guillaume Paugam <guipaugam@icloud.com>
2018-03-17 15:16:37 -0500

    The values when the results are shown are no longer undefined

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
-let randomVerb 
-let randomVerbTrans =[];
-let randomizedTrans =[];
+// Declaring variables in the global scope
+let counter;
+let intervalId;
+let randomVerb; 
+let randomVerbTrans=[];
+let randomizedTrans=[];
+$("#question_panel").hide();
+
+let gameDone = false;
 let wins = 0;
 let losses = 0;
 let games = 0;
-$("#question_panel").hide();
-
-function reset(){
+let total = 0;
+    
+function resetGame(){
+    randomVerb = ""; 
+    value = "";
     randomVerb ="";
     randomVerbTrans.length = 0;
     randomizedTrans.length = 0;
-    $("#1").val("").text("")
-    $("#2").val("").text("")
-    $("#3").val("").text("")
-    $("#4").val("").text("")
-    $("#5").val("").text("")
-    value = "";
+    $("#buttons").empty();
+    $("#question_panel").hide();
 }
 
 function newGame(){
+    counter = 10;
     randomVerb = verbArray[Math.floor(Math.random()*verbArray.length)];
     // let randomNoun = nounArray[Math.floor(Math.random()*nounArray.length)];
     // let randomNounAnswers = [];
     randomVerbTrans.push(randomVerb.englishTrans,randomVerb.badEnglishTrans1,randomVerb.badEnglishTrans2,randomVerb.badEnglishTrans3,randomVerb.badEnglishTrans4); 
     // randomNounAnswers.push(randomNoun.englishTrans,randomNoun.badEnglishTrans1,randomNoun.badEnglishTrans2); 
-    visibleTimer();
     console.log(randomVerb)
-    let j = 0;
-    while (j = 0 < randomVerbTrans.length) {
-        cut(randomVerbTrans)
-        j++;
-    }
+    randomAndCut();
     console.log(randomizedTrans);
     $("#question_panel").show();
     $("#question").val(randomVerb.frenchTrans).text(randomVerb.frenchTrans)
@@ -40,47 +40,63 @@ function newGame(){
     $("#3").val(randomizedTrans[2]).text(randomizedTrans[2])
     $("#4").val(randomizedTrans[3]).text(randomizedTrans[3])
     $("#5").val(randomizedTrans[4]).text(randomizedTrans[4])
-// need to get the value of the li element clicked (currently, always shows game over, meaning it doesn't read anything)
-    $(".option").on("click", function() {
-        var value = $(this).attr("value");
-        if (value === randomVerb.englishTrans){
-            reset();
-            gameWon();
-        }
-        else {
-            reset();
-            gameLost();
-        }
-    });
+    
+    if (gameDone === false) {
+        // visibleTimer();
+        $(".option").on("click", function() {
+            gameDone = true;
+            var value = $(this).attr("value");
+            if (value === randomVerb.englishTrans){
+                gameWon(randomVerb.frenchTrans,randomVerb.englishTrans);
+                resetGame();
+            }
+            else {
+                gameLost(randomVerb.frenchTrans,randomVerb.englishTrans);
+                resetGame();
+            }
+        });
+    }
 
 }    
-function visibleTimer(){
-      var counter = 11;
-      setInterval(function() {
-        counter--;
-        if (counter >= 0) {
-        $("#count").text(counter);
-        }
-        if (counter === 0) {
-            // other things that should be happening
-        }
-      }, 900);  
-    };
+// function visibleTimer(){
+//     gameDone=false; 
+//     setInterval(function() {
+//         counter--;
+//         if (gameDone === false) {
+//             if (counter >= 0) {
+//             $("#count").text(counter);
+//             }
+//             if (counter === 0) {
+//                 gameDone=true;            
+//                 gameLost();
+//             }
+//           } 
+//         }, 1000);  
+//     // clearInterval(intervalId);
+//     };
 
 function invisibleTimer(){
-    var counter = 6;
+    var otherCounter = 6;
     setInterval(function() {
-        counter--;
-        if (counter >= 0) {
-            console.log("Soon" + counter)
+        otherCounter--;
+        if (otherCounter >= 0) {
+            console.log("Wait " + otherCounter + " more sec before resetting the game")
         }
-        if (counter === 0) {
+        if (otherCounter === 0) {
         // start a new game
         newGame();
         }
-    }, 600);  
-    };
+    }, 600)
+}  
 
+
+function randomAndCut() {
+    let j = 0;
+    while (j = 0 < randomVerbTrans.length) {
+        cut(randomVerbTrans)
+        j++;
+    }
+}
 function cut (array) {
     option = array[Math.floor(Math.random()*array.length)];
     for (let i = 0; i < array.length; i++) {
@@ -90,21 +106,22 @@ function cut (array) {
         }
     };
 };
-function gameWon(){
+function gameWon(french,english){
     $("#question_panel").hide();
     console.log("You have won");
+    console.log(french, english)
     wins++;
     games++;
-    $(".response").text("You are correct. The French verb: \"" + randomVerb.frenchTrans + "\" means \"" + randomVerb.englishTrans +"\"")
+    $(".response").text("You are correct. The French verb: \"" + french + "\" means \"" + english +"\"")
     // happy
     invisibleTimer(); 
 }
-function gameLost(){
+function gameLost(french,english){
     $("#question_panel").hide();
     console.log("You have lost");
     losses++;
     games++;
-    $(".response").text("Actually, the French verb: \"" + randomVerb.frenchTrans + "\" means \"" + randomVerb.englishTrans +"\"")
+    $(".response").text("Actually, the French verb: \"" + french + "\" means \"" + english +"\"")
     // sad
     invisibleTimer();
 }
